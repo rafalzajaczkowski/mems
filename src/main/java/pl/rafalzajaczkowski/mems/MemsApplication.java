aee0ba8b37af14732ef4419af97fb6ccda26e20d
Rafał Zajączkowski <rafal.zajaczkowski@gmail.com>
2018-03-16 17:16:06 +0100

    open commit

---
+++ src/main/java/pl/rafalzajaczkowski/mems/MemsApplication.java
@@ -0,0 +1,12 @@
+package pl.rafalzajaczkowski.mems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemsApplication.class, args);
+	}
+}
