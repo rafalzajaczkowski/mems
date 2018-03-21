dba3062425618ba14fa2e647f6b6515283c55b92
Wiola <w.rudzka1@gmail.com>
2018-03-21 14:21:12 +0100

    upload files finished

--- src/main/java/pl/rafalzajaczkowski/mems/MemsApplication.java
+++ src/main/java/pl/rafalzajaczkowski/mems/MemsApplication.java
@@ -1,12 +1,18 @@
 package pl.rafalzajaczkowski.mems;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.web.context.embedded.tomcat.TomcatConnectorCustomizer;
+//import org.springframework.boot.web.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MemsApplication {
-
+	private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
 	public static void main(String[] args) {
 		SpringApplication.run(MemsApplication.class, args);
 	}
+	//Tomcat large file upload connection reset
 }
